7b045c634855a87c14b7d3b19ffc048912703c31
Kamil <szpmytyl@wp.pl>
2026-09-22 19:26:13 +0200

    Create NestJS products API

---
+++ src/products/products.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDTO } from './dtos/create-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  getAll() {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Post('/')
+  create(@Body() productData: CreateProductDTO) {
+    return this.productsService.create(productData);
+  }
+}
